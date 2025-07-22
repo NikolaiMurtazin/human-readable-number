a5d40e20879cf594d2f362f15ddc607c57e312d1
Nikolai Murtazin <murtazinnikol@yandex.ru>
2025-07-22 09:03:46 +0200

    feat: implement toReadable function for human-readable-number task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
-module.exports = function toReadable(/* number */) {
-  throw new Error('Not implemented');
+const ones = [
+  'zero',
+  'one',
+  'two',
+  'three',
+  'four',
+  'five',
+  'six',
+  'seven',
+  'eight',
+  'nine',
+];
+const teens = [
+  'ten',
+  'eleven',
+  'twelve',
+  'thirteen',
+  'fourteen',
+  'fifteen',
+  'sixteen',
+  'seventeen',
+  'eighteen',
+  'nineteen',
+];
+const tens = [
+  '',
+  '',
+  'twenty',
+  'thirty',
+  'forty',
+  'fifty',
+  'sixty',
+  'seventy',
+  'eighty',
+  'ninety',
+];
+
+module.exports = function toReadable(number) {
+  if (number < 10) return ones[number];
+  if (number < 20) return teens[number - 10];
+  if (number < 100) {
+    const ten = Math.floor(number / 10);
+    const rest = number % 10;
+    return tens[ten] + (rest ? ` ${ones[rest]}` : '');
+  }
+  if (number < 1000) {
+    const hundred = Math.floor(number / 100);
+    const rest = number % 100;
+    return `${ones[hundred]} hundred${rest ? ` ${toReadable(rest)}` : ''}`;
+  }
+  return '';
 };
